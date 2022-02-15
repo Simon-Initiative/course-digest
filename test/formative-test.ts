eab4d3ad6dd5e9f191f869c65750f33551dbecb3
Anders Weinstein <andersw@cs.cmu.edu>
2022-02-15 12:44:28 -0500

    undo prettier quote changes

--- test/formative-test.ts
+++ test/formative-test.ts
@@ -1,13 +1,11 @@
-import { Formative } from "../src/resources/formative";
+import { Formative } from '../src/resources/formative';
 
-const cheerio = require("cheerio");
+const cheerio = require('cheerio');
 
-describe("conversion to Torus resources", () => {
-  test("should create a Torus Activity", () => {
+describe('conversion to Torus resources', () => {
+  test('should create a Torus Activity', () => {
     new Formative(null, true)
-      .convert(
-        "./test/content/x-oli-inline-assessment/a_0cf7a2f6bb0d48cfb7202bf8794e18a4.xml"
-      )
+      .convert('./test/content/x-oli-inline-assessment/a_0cf7a2f6bb0d48cfb7202bf8794e18a4.xml')
       .then((results) => {
         expect(results.length).toEqual(1);
       });
