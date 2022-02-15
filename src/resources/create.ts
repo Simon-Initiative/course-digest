53164873f9ada3a53b1444427b22ab846866f8b1
Anders Weinstein <andersw@cs.cmu.edu>
2022-02-15 12:42:03 -0500

    undo prettier quote changes

--- src/resources/create.ts
+++ src/resources/create.ts
@@ -1,24 +1,24 @@
-import { rootTag } from "../utils/xml";
-import { Resource, ResourceType } from "./resource";
+import { rootTag } from '../utils/xml';
+import { Resource, ResourceType } from './resource';
 
-import * as WB from "./workbook";
-import * as Org from "./organization";
-import * as Other from "./other";
-import * as Feedback from "./feedback";
-import * as Formative from "./formative";
-import * as Summative from "./summative";
-import * as Objectives from "./objectives";
-import * as Pool from "./pool";
-import * as Skills from "./skills";
-import * as Superactivity from "./superactivity";
+import * as WB from './workbook';
+import * as Org from './organization';
+import * as Other from './other';
+import * as Feedback from './feedback';
+import * as Formative from './formative';
+import * as Summative from './summative';
+import * as Objectives from './objectives';
+import * as Pool from './pool';
+import * as Skills from './skills';
+import * as Superactivity from './superactivity';
 
 const minVersions: Record<string, string> = {
-  oli_workbook_page: "3_7",
-  oli_workbook_page_mathml: "3_7",
-  oli_assessment: "2_3",
-  oli_assessment_mathml: "2_3",
-  oli_inline_assessment: "1_3",
-  oli_inline_assessment_mathml: "1_3",
+  oli_workbook_page: '3_7',
+  oli_workbook_page_mathml: '3_7',
+  oli_assessment: '2_3',
+  oli_assessment_mathml: '2_3',
+  oli_inline_assessment: '1_3',
+  oli_inline_assessment_mathml: '1_3',
 };
 
 export function determineResourceType(file: string): Promise<ResourceType> {
@@ -27,77 +27,70 @@ export function determineResourceType(file: string): Promise<ResourceType> {
     var [, dtdBase, dtdVersion] = /\/dtd\/([^\d]+)_(\d.*)\.dtd/.exec(tag) || [];
 
     // lexicographic string comparison on version suffix suffices
-    var minVersion = minVersions[dtdBase] || "";
+    var minVersion = minVersions[dtdBase] || '';
     if (dtdVersion < minVersion) {
       console.error(`unsupported DTD version: ${dtdBase} ${dtdVersion}`);
-      return "Other";
+      return 'Other';
     }
 
-    if (tag.indexOf("oli_skills_model") !== -1) {
-      return "Skills";
+    if (tag.indexOf('oli_skills_model') !== -1) {
+      return 'Skills';
     }
-    if (tag.indexOf("DTD Assessment Pool") !== -1) {
-      return "Pool";
+    if (tag.indexOf('DTD Assessment Pool') !== -1) {
+      return 'Pool';
     }
-    if (tag.indexOf("organization") !== -1) {
-      return "Organization";
+    if (tag.indexOf('organization') !== -1) {
+      return 'Organization';
     }
-    if (tag.indexOf("oli_workbook_page") !== -1) {
-      return "WorkbookPage";
+    if (tag.indexOf('oli_workbook_page') !== -1) {
+      return 'WorkbookPage';
     }
-    if (tag.indexOf("oli_inline_assessment") !== -1) {
-      return "Formative";
+    if (tag.indexOf('oli_inline_assessment') !== -1) {
+      return 'Formative';
     }
-    if (tag.indexOf("oli_assessment") !== -1) {
-      return "Summative";
+    if (tag.indexOf('oli_assessment') !== -1) {
+      return 'Summative';
     }
-    if (tag.indexOf("oli_feedback") !== -1) {
-      return "Feedback";
+    if (tag.indexOf('oli_feedback') !== -1) {
+      return 'Feedback';
     }
-    if (tag.indexOf("oli_learning_objectives") !== -1) {
-      return "Objectives";
+    if (tag.indexOf('oli_learning_objectives') !== -1) {
+      return 'Objectives';
     }
-    if (
-      tag.indexOf("oli-embed-activity") !== -1 ||
-      tag.indexOf("oli-linked-activity") !== -1
-    ) {
-      return "Superactivity";
+    if (tag.indexOf('oli-embed-activity') !== -1 || tag.indexOf('oli-linked-activity') !== -1) {
+      return 'Superactivity';
     }
 
-    return "Other";
+    return 'Other';
   });
 }
 
-export function create(
-  t: ResourceType,
-  file: string,
-  navigable: boolean
-): Resource {
-  if (t === "WorkbookPage") {
+export function create(t: ResourceType, file: string, navigable: boolean): Resource {
+  if (t === 'WorkbookPage') {
     return new WB.WorkbookPage(file, navigable);
   }
-  if (t === "Organization") {
+  if (t === 'Organization') {
     return new Org.Organization(file, navigable);
   }
-  if (t === "Formative") {
+  if (t === 'Formative') {
     return new Formative.Formative(file, navigable);
   }
-  if (t === "Summative") {
+  if (t === 'Summative') {
     return new Summative.Summative(file, navigable);
   }
-  if (t === "Feedback") {
+  if (t === 'Feedback') {
     return new Feedback.Feedback(file, navigable);
   }
-  if (t === "Objectives") {
+  if (t === 'Objectives') {
     return new Objectives.Objectives(file, navigable);
   }
-  if (t === "Superactivity") {
+  if (t === 'Superactivity') {
     return new Superactivity.Superactivity(file, navigable);
   }
-  if (t === "Pool") {
+  if (t === 'Pool') {
     return new Pool.Pool(file, navigable);
   }
-  if (t === "Skills") {
+  if (t === 'Skills') {
     return new Skills.Skills(file, navigable);
   }
   return new Other.Other(file, navigable);
