9eaab110e6c29ea34900284bcbc13b46e072a9c1
Anders Weinstein <andersw@cs.cmu.edu>
2023-07-18 19:02:49 -0400

    new webcontent handling: optionally upload whole tree into bundle space

--- src/resources/webcontent.ts
+++ src/resources/webcontent.ts
@@ -1,5 +1,4 @@
 import { MediaSummary, ProcessedMediaItem } from 'src/media';
-import { guid } from 'src/utils/common';
 import * as glob from 'glob';
 import * as fs from 'fs';
 import * as path from 'path';
@@ -14,74 +13,53 @@ export function addWebContentToMediaSummary(
   webContentBundle?: string
 ): Promise<MediaSummary> {
   return new Promise((resolve, _reject) => {
+    // no-op if not webcontent bundle not requested on command line
+    if (!webContentBundle) {
+      resolve(summary);
+      return;
+    }
+
     glob(
       `${directory}/**/webcontent/**/*`,
       { nodir: true },
       (err: any, files: string[]) => {
+        console.log('webcontent file count: ' + files.length);
         // Sort file paths by depth to ensure that shallower paths are processed first when a
         // filename conflict, in the now relatively flatted webcontent file structure, is detected
+        // (This no longer necessary, but gives reasonable order)
         const sortedFiles = [...files].sort((a: string, b: string) => {
           return a.split('/').length - b.split('/').length;
         });
 
-        sortedFiles.forEach((file: string) => {
+        const items = sortedFiles.map((file: string) => {
           const absolutePath = path.resolve(file);
           const name = path.basename(absolutePath);
+          const relativePath = getPathRelativeToContentRoot(
+            projectSummary.packageDirectory,
+            absolutePath
+          );
           const md5 = md5File.sync(absolutePath);
-          const subdir = md5.substring(0, 2);
-
-          if (summary.mediaItems[absolutePath] === undefined) {
-            const flattenedName = summary.flattenedNames[name]
-              ? generateNewName(name)
-              : name;
-
-            summary.flattenedNames[flattenedName] = flattenedName;
-            summary.mediaItems[absolutePath] = {
-              file: absolutePath,
-              fileSize: getFilesizeInBytes(absolutePath),
-              name,
-              flattenedName,
-              md5,
-              mimeType: mime.lookup(absolutePath) || 'application/octet-stream',
-              references: [],
-              url: `${summary.urlPrefix}/${subdir}/${md5}/${flattenedName}`,
-            };
-          }
+          return {
+            file: absolutePath,
+            fileSize: getFilesizeInBytes(absolutePath),
+            name,
+            md5,
+            mimeType: mime.lookup(absolutePath) || 'application/octet-stream',
+            url: `${summary.urlPrefix}/bundles/${webContentBundle}/${relativePath}`,
+          };
         });
 
-        if (webContentBundle) {
-          // Create a webcontent bundle of the directory hierarchy if webContentBundle is specified
-          const items = files.map((file: string) => {
-            const absolutePath = path.resolve(file);
-            const name = path.basename(absolutePath);
-            const relativePath = getPathRelativeToPackage(
-              projectSummary.packageDirectory,
-              absolutePath
-            );
-            const md5 = md5File.sync(absolutePath);
+        const totalSize = items.reduce(
+          (acc: number, item: ProcessedMediaItem) => acc + item.fileSize,
+          0
+        );
 
-            return {
-              file: absolutePath,
-              fileSize: getFilesizeInBytes(absolutePath),
-              name,
-              md5,
-              mimeType: mime.lookup(absolutePath) || 'application/octet-stream',
-              url: `${summary.urlPrefix}/webcontent/${webContentBundle}/${relativePath}`,
-            };
-          });
-
-          const totalSize = items.reduce(
-            (acc: number, item: ProcessedMediaItem) => acc + item.fileSize,
-            0
-          );
-
-          summary.webContentBundle = {
-            name: webContentBundle,
-            url: `${summary.urlPrefix}/webcontent/${webContentBundle}`,
-            items,
-            totalSize,
-          };
-        }
+        summary.webContentBundle = {
+          name: webContentBundle,
+          url: `${summary.urlPrefix}/webcontent/${webContentBundle}`,
+          items,
+          totalSize,
+        };
 
         resolve(summary);
       }
@@ -89,18 +67,13 @@ export function addWebContentToMediaSummary(
   });
 }
 
-const getPathRelativeToPackage = (
+const getPathRelativeToContentRoot = (
   packageDirectory: string,
   absolutePath: string
-) => path.relative(packageDirectory, absolutePath);
+) => path.relative(packageDirectory + '/content', absolutePath);
 
 function getFilesizeInBytes(filename: string) {
   const stats = fs.statSync(filename);
   const fileSizeInBytes = stats.size;
   return fileSizeInBytes;
-}
-
-function generateNewName(name: string) {
-  const final = name.replace('webcontent/', `webcontent/${guid()}/`);
-  return final;
 }
