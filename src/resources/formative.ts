99a3f9133adf0361d1b977c22b9531f402dbce7e
Anders Weinstein <andersw@cs.cmu.edu>
2024-02-21 17:12:20 -0500

    dont include correct answer in targeted feedback

--- src/resources/formative.ts
+++ src/resources/formative.ts
@@ -107,9 +107,9 @@ function buildMCQPart(question: any) {
   }
 
   const targeted: any = [];
-
+  const correctId = r.find((r: any) => r.score > 0)?.id;
   r.forEach((r: any) => {
-    if (r.legacyMatch !== '.*') {
+    if (r.legacyMatch !== '.*' && r.id !== correctId) {
       targeted.push([[r.legacyMatch], r.id]);
     }
   });
